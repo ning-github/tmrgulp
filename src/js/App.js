46c96bdb28d7e253df33b639d5088e4b4f98154a
Ning Xia <nxiamail@gmail.com>
2015-07-15 14:21:21 -0700

    set up intial file structure

---
+++ src/js/App.js
@@ -0,0 +1,6 @@
+// var React = require("react");
+var Parent = require('/Parent');
+var Child = require('./Child');
+
+
+React.render(<Parent />, document.getElementById('app'));
---
+++ src/js/Parent.js
@@ -0,0 +1,13 @@
+// var React = require("react");
+var Child = require('./Child.js');
+
+var Parent = React.createClass({
+  render: function(){
+    return (
+      <div>
+        <div> This is the parent. </div>
+        <Child name="child"/>
+      </div>
+    )
+  }
+});
